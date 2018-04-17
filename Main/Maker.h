865cd815d5ab96b0da26a4630093899e0a71af65
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2018-04-17 17:51:40 -0500

    up

--- Main/Maker.h
+++ Main/Maker.h
@@ -139,7 +139,7 @@ namespace Main {
     const bool _makePlots = false;
 
     //const bool _fill_bootstrap = true;
-    const bool _fill_bootstrap_flux = false;
+    const bool _fill_bootstrap_flux = true;
     const bool _fill_bootstrap_genie = false;
 
     std::string _target_flux_syst = "";
