533ccbd1bc59278ff426e1f9de867b0bd8b58551
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2018-11-09 18:37:55 -0600

    cleaning up drivers

--- Main/mac/analyse.py
+++ Main/mac/analyse.py
@@ -10,18 +10,13 @@ analyser = Main.Analyse()
 
 # analyser.SetBNBCosmicFile     ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_bnbcosmic_ubcodev06_26_01_22__v2_nosyst.root") # Tune 1
 analyser.SetBNBCosmicFile     ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_bnbcosmic_ubcodev06_26_01_22__v4.root") # Tune 1 - full stat - full syst
-analyser.SetBNBCosmicFile     ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_bnbcosmic_ubcodev06_26_01_22__v5.root") # Tune 1 - dev - full syst
+# analyser.SetBNBCosmicFile     ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_bnbcosmic_ubcodev06_26_01_22__v5.root") # Tune 1 - dev - full syst
 # analyser.SetBNBCosmicFile     ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_bnbcosmic_tune3_full_ubcodev06_26_01_22__v3.root") # Tune 3 (full)
 
 analyser.SetDirtFile          ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_mc_dirt_ubcodev06_26_01_22__v3.root")
 
 analyser.SetInTimeCosmicFile  ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_bnbcosmic_mcc8.7_test6.root") # Just a placeholder
 
-# analyser.SetBNBONFile         ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_data_bnbon_run1_ubcodev06_26_01_22__v1.root")    
-# analyser.SetEXTBNBFile        ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_data_extbnb_run1_ubcodev06_26_01_22__v1.root")
-# analyser.SetBNBPOT(1.159e+20)    
-# analyser.SetBNBONTriggers(25848208.0)   
-# analyser.SetEXTBNBTriggers(28745873)
 analyser.SetBNBONFile         ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_data_bnbon_run1_ubcodev06_26_01_22__v4.root")    
 analyser.SetEXTBNBFile        ("/Users/deltutto/CCInclusiveFiles/Output/ubxsecana_output_data_extbnb_run1_ubcodev06_26_01_22__v4.root")
 analyser.SetBNBPOT(1.592e+20)    
@@ -37,7 +32,7 @@ analyser.ImportAlternativeMC("xsec_file_cv_tune3.root")
 analyser.SetBeamOffSubtraction(False)
 analyser.SetBreakdownPlots(True)
 
-extra_unc = math.sqrt(0.02*0.02 + 0.04*0.04) # POT counting, beam window
+extra_unc = math.sqrt(0.02*0.02) # POT counting
 # analyser.SetExtraUncertainty(extra_unc)
 
 analyser.ImportDetectorSystematics(False)
