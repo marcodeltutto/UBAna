a6af53402de5762730e066aa6e5e99debaaabee7
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2019-01-03 08:36:46 +0100

    adding truth cut to remove flipping tracks

--- Main/Maker.cxx
+++ Main/Maker.cxx
@@ -2424,6 +2424,9 @@ void Main::Maker::MakeFile()
 
     // if (t->slc_muoncandidate_mom_mcs.at(scl_ll_max) > 2.5) continue;
 
+    // Remove flipped tracks (truth cut)
+    if (isSignal && (t->lep_costheta * t->slc_longesttrack_theta.at(scl_ll_max) < 0) ) continue;
+
 
     
     
